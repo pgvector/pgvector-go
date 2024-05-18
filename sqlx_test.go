1db3ffc6be7ebc26ee2cfb8235c3e4552d8b88a0
Andrew Kane <andrew@ankane.org>
2024-05-18 16:08:22 -0400

    Added test for bit with sqlx [skip ci]

--- sqlx_test.go
+++ sqlx_test.go
@@ -13,17 +13,18 @@ type SqlxItem struct {
 	Id              int64
 	Embedding       pgvector.Vector
 	HalfEmbedding   pgvector.HalfVector
+	BinaryEmbedding string
 	SparseEmbedding pgvector.SparseVector
 }
 
 func CreateSqlxItems(db *sqlx.DB) {
 	items := []SqlxItem{
-		SqlxItem{Embedding: pgvector.NewVector([]float32{1, 1, 1}), HalfEmbedding: pgvector.NewHalfVector([]float32{1, 1, 1}), SparseEmbedding: pgvector.NewSparseVector([]float32{1, 1, 1})},
-		SqlxItem{Embedding: pgvector.NewVector([]float32{2, 2, 2}), HalfEmbedding: pgvector.NewHalfVector([]float32{2, 2, 2}), SparseEmbedding: pgvector.NewSparseVector([]float32{2, 2, 2})},
-		SqlxItem{Embedding: pgvector.NewVector([]float32{1, 1, 2}), HalfEmbedding: pgvector.NewHalfVector([]float32{1, 1, 2}), SparseEmbedding: pgvector.NewSparseVector([]float32{1, 1, 2})},
+		SqlxItem{Embedding: pgvector.NewVector([]float32{1, 1, 1}), HalfEmbedding: pgvector.NewHalfVector([]float32{1, 1, 1}), BinaryEmbedding: "000", SparseEmbedding: pgvector.NewSparseVector([]float32{1, 1, 1})},
+		SqlxItem{Embedding: pgvector.NewVector([]float32{2, 2, 2}), HalfEmbedding: pgvector.NewHalfVector([]float32{2, 2, 2}), BinaryEmbedding: "101", SparseEmbedding: pgvector.NewSparseVector([]float32{2, 2, 2})},
+		SqlxItem{Embedding: pgvector.NewVector([]float32{1, 1, 2}), HalfEmbedding: pgvector.NewHalfVector([]float32{1, 1, 2}), BinaryEmbedding: "111", SparseEmbedding: pgvector.NewSparseVector([]float32{1, 1, 2})},
 	}
 
-	_, err := db.NamedExec(`INSERT INTO sqlx_items (embedding, halfembedding, sparseembedding) VALUES (:embedding, :halfembedding, :sparseembedding)`, items)
+	_, err := db.NamedExec(`INSERT INTO sqlx_items (embedding, halfembedding, binaryembedding, sparseembedding) VALUES (:embedding, :halfembedding, :binaryembedding, :sparseembedding)`, items)
 	if err != nil {
 		panic(err)
 	}
@@ -35,7 +36,7 @@ func TestSqlx(t *testing.T) {
 	db.MustExec("CREATE EXTENSION IF NOT EXISTS vector")
 	db.MustExec("DROP TABLE IF EXISTS sqlx_items")
 
-	db.MustExec("CREATE TABLE sqlx_items (id bigserial PRIMARY KEY, embedding vector(3), halfembedding halfvec(3), sparseembedding sparsevec(3))")
+	db.MustExec("CREATE TABLE sqlx_items (id bigserial PRIMARY KEY, embedding vector(3), halfembedding halfvec(3), binaryembedding bit(3), sparseembedding sparsevec(3))")
 
 	db.MustExec("CREATE INDEX ON sqlx_items USING hnsw (embedding vector_l2_ops)")
 
@@ -51,5 +52,8 @@ func TestSqlx(t *testing.T) {
 	}
 	if !reflect.DeepEqual(items[1].HalfEmbedding.Slice(), []float32{1, 1, 2}) {
 		t.Errorf("Bad half embedding")
+	}
+	if items[0].BinaryEmbedding != "000" || items[1].BinaryEmbedding != "111" || items[2].BinaryEmbedding != "101" {
+		t.Errorf("Bad binary embeddings")
 	}
 }
